01333b02364501801d415ce589b0a2e74209f0d0
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-12 21:32:12 +0100

    creation viewMain.php

--- index.php
+++ index.php
@@ -14,45 +14,8 @@
 
     $posts = $db->query($strSQL);
     $posts->setFetchMode(PDO::FETCH_OBJ);
-?>
+
+require 'viewMain.php';
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>Welcome on my blog</title>
-</head>
-<body>
-    <div id="global">
-        <header>
-            <a href="">
-                <h1 id="blog_title">My Blog</h1>
-            </a>
-            <p>Welcome on this simple blog !</p>
-        </header>
-        <div id="content">
-        
 
-        <?php foreach ($posts as $post) : ?>
-            <article>
-                <header>
-                    <h1 class="post_title"><?php echo $post->title; ?></h1>
-                    <time><?php echo $post->date; ?></time>
-                </header>
-                <p><?php echo $post->content; ?></p>
-            </article>
-            <hr>
-        <?php endforeach; ?>
-
-            
-        </div> <!-- #content -->
-        
-        <footer id="blog_footer">
-            Blog performed with PHP, HTML5 and CSS.
-        </footer>
-
-    </div> <!-- #global -->
-</body>
-</html>
