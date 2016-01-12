4baf2e41b73c983d876e00472e7429d92238a738
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-12 21:37:22 +0100

    creation of model.php

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
-    //Access to the database using PDO
-    $db = new PDO(
-        'mysql:host=localhost;dbname=BLOG_MVC;charset=utf8',
-        'root',
-        'root'
-    );
 
-    //This is the query to get the different posts for the main page
-    $strSQL = 'SELECT POST_ID as id, POST_DATE as date,            ' .
-              '       POST_TITLE as title, POST_CONTENT as content ' .
-              'FROM T_POST                                         ' .
-              'ORDER BY POST_ID DESC                               ';
+require 'model.php';    
 
-    $posts = $db->query($strSQL);
-    $posts->setFetchMode(PDO::FETCH_OBJ);
+$posts = getPosts();   
 
 require 'viewMain.php';
 
