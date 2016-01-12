56a42013fb426d61f21ea275868948a7ccfde0dd
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-12 21:26:01 +0100

    isolation of database connexion

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+    //Access to the database using PDO
+    $db = new PDO(
+        'mysql:host=localhost;dbname=BLOG_MVC;charset=utf8',
+        'root',
+        'root'
+    );
+
+    //This is the query to get the different posts for the main page
+    $strSQL = 'SELECT POST_ID as id, POST_DATE as date,            ' .
+              '       POST_TITLE as title, POST_CONTENT as content ' .
+              'FROM T_POST                                         ' .
+              'ORDER BY POST_ID DESC                               ';
+
+    $posts = $db->query($strSQL);
+    $posts->setFetchMode(PDO::FETCH_OBJ);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,21 +33,7 @@
             <p>Welcome on this simple blog !</p>
         </header>
         <div id="content">
-        <?php
-            $db = new PDO(
-                'mysql:host=localhost;dbname=BLOG_MVC;charset=utf8',
-                'root',
-                'root'
-            );
-
-            $strSQL = 'SELECT POST_ID as id, POST_DATE as date,            ' .
-                      '       POST_TITLE as title, POST_CONTENT as content ' .
-                      'FROM T_POST                                         ' .
-                      'ORDER BY POST_ID DESC                               ';
-
-            $posts = $db->query($strSQL);
-            $posts->setFetchMode(PDO::FETCH_OBJ);
-        ?>
+        
 
         <?php foreach ($posts as $post) : ?>
             <article>
