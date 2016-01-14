c290045073b3264e8f45f843d939b60da8a5244b
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-14 20:35:53 +0100

    creation of a function to connect to the database

--- model.php
+++ model.php
@@ -7,12 +7,7 @@
 function getPosts() 
 {   
 
-    //Access to the database using PDO
-    $db = new PDO(
-        'mysql:host=localhost;dbname=BLOG_MVC;charset=utf8',
-        'root',
-        'root'
-    );
+   $db = getDb(); 
 
     //This is the query to get the different posts for the main page
     $strSQL = 'SELECT POST_ID as id, POST_DATE as date,            ' .
@@ -24,4 +19,20 @@ function getPosts()
     $posts->setFetchMode(PDO::FETCH_OBJ);
 
     return $posts;
+}
+
+/**
+ * This function is used to create a connexion to the database with PDO
+ * @return PDO Object
+ */
+function getDb()
+{
+    //Access to the database using PDO
+    $db = new PDO(
+        'mysql:host=localhost;dbname=BLOG_MVC;charset=utf8',
+        'root',
+        'root'
+    );
+
+    return $db;
 }
