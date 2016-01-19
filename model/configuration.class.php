883a693143b94b146542c2b96a431c189eb2c500
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-19 17:41:05 +0100

    creation of configuration class to get parameters from a file config

---
+++ model/configuration.class.php
@@ -0,0 +1,49 @@
+<?php 
+
+/**
+* This class manage configuration in order to get parameters 
+* from a file config to connect to the database
+*/
+class Configuration
+{
+	private static $parameters;
+
+	/**
+	 * Return the value of a config parameter
+	 */
+	public static function get($name, $defaultValue = null)
+	{
+		if (isset(self::getParameters()[$name])) {
+			$value = self::getParameters()[$name];
+		} else {
+			$value = $defaultValue;
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Return all the parameters of the app to connect to the database
+	 * Load the file if necessary
+	 */
+	private function getParameters()
+	{
+		if(self::$parameters == null) {
+			
+			$filePath = "configuration/prod.ini";
+
+			if (file_exists($filePath) == false) {
+				$filePath = "configuration/dev.ini"
+			}
+
+			if (file_exists($filePath) == false) {
+				throw new Exception("No configuration file found...");
+			}
+
+		} else {
+			self::$parameters = parse_ini_file($filePath);
+		}
+
+		return self::$parameters;
+	}
+}
