1096480ff8b432f664857c16f46be6a59d706422
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-20 14:53:03 +0100

    creation of the Request Class in order to model an HTTP request

---
+++ controller/request.class.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+* The aim of this class is to model an HTTP request
+*/
+class Request
+{
+	/**
+	 * Parammeters of the request
+	 */
+	private $requestParams
+
+	function __construct($params)
+	{
+		$this->requestParams = $params;
+	}
+
+	/**
+	 * return true if the parameter exists in the HTTP request
+	 */
+	public function requestParamExist($name)
+	{
+		return (isset($this->requestParams[$name]) 
+			    && $this->requestParams[$name] != "");
+	}
+
+	/**
+	 * return the value of the parameter asked
+	 * throw an exception if the parameter $name is not found
+	 */
+	public function requestGetParam($name)
+	{
+		if(requestParamExist($name)) {
+			return $this->requestParams[$name];
+		} else {
+			throw new Exception("'$name' parameter does not exist in the request...");
+		}
+	}
+}
--- controller/router.class.php
+++ controller/router.class.php
@@ -1,74 +1,35 @@
 <?php 
 
-require_once 'controller/controllerWelcome.class.php';
-require_once 'controller/controllerPost.class.php';
+require_once 'request.class.php';
 require_once 'view/view.class.php';
 
 class router 
 {
 
-    private $ctrlWelcome;
-    private $ctrlPost;
-
-
     /**
-     * Class Constructor
+     * Route an incoming Request : execute the associated action
      */
-    public function __construct()
-    {
-        $this->ctrlWelcome = new controllerWelcome();
-        $this->ctrlPost    = new controllerPost();
-    }
-
     public function routerRequest()
     {
         try {
-            if (isset($_GET['action'])) {
+            $request    = new Request($_Request);
+            $controller = $this->routerCreateController($request);
+            $action     = $this->routerCreateAction($request);
 
-                //Call Post controller to get specific post
-                if($_GET['action'] == 'post') {
-                    $idpost = intval($this->getParameter($_GET, "id"));
-                    if ($idpost != 0) {
-                        $this->ctrlPost->post($idpost);
-                    } else {
-                        throw new Exception("ID post is not valid");
-                    }
-                }
-                //Call Comment controller in order to save a comment
-                else if($_GET['action'] == 'comment') {
-                    $author  = $this->getParameter($_POST, "author");
-                    $content = $this->getParameter($_POST, "content");
-                    $idPost  = $this->getParameter($_POST, "idpost");
-                    $this->ctrlPost->comment($author, $content, $idPost);
-                }
+            //Execution of the action
+            $controller->ctrlExecuteAction($action);
 
-                else {
-                    throw new Exception("Not valid action");
-                }
-            }
-
-            // No action GET variable, so it's the default page
-            else {
-                $this->ctrlWelcome->welcome();
-            }
-
-        } catch(Exception $e) {
-            $this->routerError($e->getMessage());
+        } catch (Exception $e) {
+            $this->routerError($e);
         }
     }
 
-    private function routerError($msgError)
+
+
+    private function routerError(Exception $exception)
     {
         $view = new View("viewError");
-        $view->generate(array('msgError' => $msgError));
+        $view->generate(array('msgError' => $exception->getMessage()));
     }
 
-    private function getParameter($tab, $name)
-    {
-        if(isset($tab[$name])) {
-            return $tab[$name];
-        } else {
-            throw new Exception("Parameter " . $name . " is missing.");
-        }
-    }
 }
